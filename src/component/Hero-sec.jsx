453f610feafbeb9e9ef15c3fa5c998da6a25191d
huzaifa-codes <huzaifabusiness60@gmail.com>
2025-06-05 22:10:36 +0500

    Huzaifa-Portfolio

--- src/component/Hero-sec.jsx
+++ src/component/Hero-sec.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 export default function Herosec() {
   return (
     <div className='mt-20'>
-      <h1 className='text-center text-6xl md:text-7xl' style={{ fontFamily: 'Playfair Display' }}>
-        <span className='text-5xl md:text-7xl'>Muhammad Huzaifa!</span><br />
+      <h1 className='text-center text-3xl  sm:text-5xl md:text-7xl' style={{ fontFamily: 'Playfair Display' }}>
+        <span className='text-4xl  sm:text-5xl md:text-7xl'>Muhammad Huzaifa!</span><br />
         Web & Mobile App Developer <br />
-        <span className='text-5xl md:text-7xl'>Learning. Building. Growing.</span>
+        <span className='text-4xl  sm:text-5xl md:text-7xl'>Learning. Building. Growing.</span>
       </h1>
     </div>
   )
-}
+}  
